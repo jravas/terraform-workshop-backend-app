f7fa954027b4f859e46f411994ad0a072e9b54eb
jravas <josip@prototyp.digital>
2024-04-09 17:44:21 +0200

    Added database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+  ConfigModule.forRoot(),
+  SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    autoLoadModels: true,
+    synchronize: true,
+  }),
+  UsersModule
+]
 })
 export class AppModule {}
